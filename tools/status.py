a008ac90ad1709c2e3f45a6f638bb6c828ff654c
Stefan van der Walt <stefanv@berkeley.edu>
2021-04-15 14:46:22 -0700

    Make Fritz CLI less noisy

--- tools/status.py
+++ tools/status.py
@@ -1,15 +1,42 @@
 from contextlib import contextmanager
 import sys
+import io
 
 
 @contextmanager
-def status(message):
-    print(f"[·] {message}", end="")
+def redirect_std(out):
+    """
+    Contextmanager to temporarily redirect stdout toa StringIO object.
+
+    """
     sys.stdout.flush()
+    stdout = sys.stdout
     try:
+        sys.stdout = out
         yield
+    finally:
+        sys.stdout = stdout
+
+
+@contextmanager
+def status(message, quiet=True):
+    print(f"[·] {message}", end="")
+
+    fake_stdout = io.StringIO()
+
+    try:
+        with redirect_std(fake_stdout):
+            yield
     except Exception:
         print(f"\r[✗] {message}")
+
+        fake_stdout.seek(0)
+        out = fake_stdout.read().strip()
+        if out:
+            print("-- captured output --")
+            print(out)
+            print("-- end output --")
+
         raise
     else:
         print(f"\r[✓] {message}")
